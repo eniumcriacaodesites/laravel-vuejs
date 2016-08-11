54dbf275908d4654228560e32bb91242a644a122
netoudi <netoudi@rocketmail.com>
2016-08-11 17:25:08 -0300

    Atualizando um conta.

--- vuejs/app.js
+++ vuejs/app.js
@@ -7,6 +7,7 @@ var app = new Vue({
             {id: 1, name: "Criar conta"}
         ],
         activedView: 0,
+        formType: 'insert',
         bill: {
             date_due: '',
             name: '',
@@ -23,11 +24,11 @@ var app = new Vue({
             'Gasolina'
         ],
         bills: [
-            {date_due: '11/08/16', name: 'Água', value: 55.99, done: 1},
-            {date_due: '11/08/16', name: 'Energia', value: 130.95, done: 0},
-            {date_due: '15/08/16', name: 'Telefone', value: 75.95, done: 0},
+            {date_due: '11/08/16', name: 'Conta de água', value: 55.99, done: 1},
+            {date_due: '11/08/16', name: 'Conta de luz', value: 130.95, done: 0},
+            {date_due: '15/08/16', name: 'Conta de telefone', value: 75.95, done: 0},
             {date_due: '15/08/16', name: 'Cartão de crédito', value: 1350.85, done: 0},
-            {date_due: '20/08/16', name: 'Empréstino', value: 2000.15, done: 0},
+            {date_due: '20/08/16', name: 'Empréstimo', value: 2000.15, done: 0},
             {date_due: '20/08/16', name: 'Supermecado', value: 650.45, done: 0},
             {date_due: '20/08/16', name: 'Gasolina', value: 150.25, done: 0},
         ]
@@ -46,10 +47,26 @@ var app = new Vue({
     methods: {
         viewShow: function (id) {
             this.activedView = id;
+            if (id == 1) {
+                this.formType = 'insert';
+            }
         },
         submit: function () {
-            this.bills.push(this.bill);
+            if (this.formType == 'insert') {
+                this.bills.push(this.bill);
+            }
+            this.bill = {
+                date_due: '',
+                name: '',
+                value: 0,
+                done: 0
+            };
             this.activedView = 0;
+        },
+        loadBill: function (bill) {
+            this.bill = bill;
+            this.activedView = 1;
+            this.formType = 'update';
         }
     }
 });
