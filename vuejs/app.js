ca7af1736422dc002e8e92bf6b24bdb6713e24cf
netoudi <netoudi@rocketmail.com>
2016-08-18 10:18:51 -0300

    Resolução da fase de projeto.

--- vuejs/app.js
+++ vuejs/app.js
@@ -12,7 +12,7 @@ var app = new Vue({
             date_due: '',
             name: '',
             value: 0,
-            done: 0
+            done: false
         },
         names: [
             'Conta de luz',
@@ -24,19 +24,19 @@ var app = new Vue({
             'Gasolina'
         ],
         bills: [
-            {date_due: '11/08/16', name: 'Conta de água', value: 55.99, done: 1},
-            {date_due: '11/08/16', name: 'Conta de luz', value: 130.95, done: 0},
-            {date_due: '15/08/16', name: 'Conta de telefone', value: 75.95, done: 0},
-            {date_due: '15/08/16', name: 'Cartão de crédito', value: 1350.85, done: 0},
-            {date_due: '20/08/16', name: 'Empréstimo', value: 2000.15, done: 0},
-            {date_due: '20/08/16', name: 'Supermecado', value: 650.45, done: 0},
-            {date_due: '20/08/16', name: 'Gasolina', value: 150.25, done: 0},
+            {date_due: '11/08/16', name: 'Conta de água', value: 55.99, done: true},
+            {date_due: '11/08/16', name: 'Conta de luz', value: 130.95, done: false},
+            {date_due: '15/08/16', name: 'Conta de telefone', value: 75.95, done: false},
+            {date_due: '15/08/16', name: 'Cartão de crédito', value: 1350.85, done: false},
+            {date_due: '20/08/16', name: 'Empréstimo', value: 2000.15, done: false},
+            {date_due: '20/08/16', name: 'Supermecado', value: 650.45, done: false},
+            {date_due: '20/08/16', name: 'Gasolina', value: 150.25, done: false},
         ]
     },
     computed: {
         status: function () {
             var count = 0;
-            if (this.bills.length === 0) {
+            if (!this.bills.length) {
                 return false;
             }
             for (var i in this.bills) {
@@ -62,7 +62,7 @@ var app = new Vue({
                 date_due: '',
                 name: '',
                 value: 0,
-                done: 0
+                done: false
             };
             this.activedView = 0;
         },
@@ -71,14 +71,14 @@ var app = new Vue({
             this.activedView = 1;
             this.formType = 'update';
         },
-        deleteBill: function (index) {
-            if (confirm('Deseja realmente deletar a conta?')) {
-                this.bills.splice(index, 1);
+        deleteBill: function (bill) {
+            if (confirm('Deseja excluir está conta?')) {
+                this.bills.$remove(bill);
             }
         },
         payBill: function (bill) {
-            var message = (bill.done == 1) ? "Deseja realmente mudar o status da conta para 'Não paga'?"
-                : "Deseja realmente mudar o status da conta para 'Paga'?";
+            var message = (bill.done == 1) ? "Deseja mudar o status desta conta para 'Não paga'?"
+                : "Deseja mudar o status desta conta para 'Paga'?";
             if (confirm(message)) {
                 bill.done = !bill.done;
             }
