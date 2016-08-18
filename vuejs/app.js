1089fad41f56fed5b91b8b935d6a03c5d687c471
netoudi <netoudi@rocketmail.com>
2016-08-18 11:59:30 -0300

    Criando BillListComponent.

--- vuejs/app.js
+++ vuejs/app.js
@@ -28,6 +28,77 @@ var menuComponent = Vue.extend({
 
 Vue.component('menu-component', menuComponent);
 
+var billListComponent = Vue.extend({
+    template: `
+        <table border="1" cellpadding="10">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Vencimento</th>
+                <th>Nome</th>
+                <th>Valor</th>
+                <th>Paga?</th>
+                <th>Ações</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(index, o) in bills">
+                <td>{{ index + 1 }}</td>
+                <td>{{ o.date_due }}</td>
+                <td>{{ o.name }}</td>
+                <td>{{ o.value | currency 'R$ ' 2 }}</td>
+                <td class="my-class" :class="{'green': o.done, 'red': !o.done}">
+                    <div v-if="o.done === 1">
+                        <a href="#" @click.prevent="payBill(o)">{{ o.done | doneLabel }}</a>
+                    </div>
+                    <div v-else>
+                        <a href="#" @click.prevent="payBill(o)">{{ o.done | doneLabel }}</a>
+                    </div>
+                </td>
+                <td>
+                    <a href="#" @click.prevent="loadBill(o)">Editar</a> |
+                    <a href="#" @click.prevent="deleteBill(o)">Excluir</a>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    `,
+    data: function () {
+        return {
+            bills: [
+                {date_due: '11/08/16', name: 'Conta de água', value: 55.99, done: true},
+                {date_due: '11/08/16', name: 'Conta de luz', value: 130.95, done: false},
+                {date_due: '15/08/16', name: 'Conta de telefone', value: 75.95, done: false},
+                {date_due: '15/08/16', name: 'Cartão de crédito', value: 1350.85, done: false},
+                {date_due: '20/08/16', name: 'Empréstimo', value: 2000.15, done: false},
+                {date_due: '20/08/16', name: 'Supermecado', value: 650.45, done: false},
+                {date_due: '20/08/16', name: 'Gasolina', value: 150.25, done: false},
+            ]
+        };
+    },
+    methods: {
+        loadBill: function (bill) {
+            this.bill = bill;
+            this.$parent.activedView = 1;
+            this.$parent.formType = 'update';
+        },
+        deleteBill: function (bill) {
+            if (confirm('Deseja excluir está conta?')) {
+                this.bills.$remove(bill);
+            }
+        },
+        payBill: function (bill) {
+            var message = (bill.done == 1) ? "Deseja mudar o status desta conta para 'Não paga'?"
+                : "Deseja mudar o status desta conta para 'Paga'?";
+            if (confirm(message)) {
+                bill.done = !bill.done;
+            }
+        }
+    }
+});
+
+Vue.component('bill-list-component', billListComponent);
+
 var appComponent = Vue.extend({
     template: `
         <style type="text/css">
@@ -53,38 +124,7 @@ var appComponent = Vue.extend({
         <menu-component></menu-component>
         
         <div v-if="activedView == 0 && bills.length > 0">
-            <table border="1" cellpadding="10">
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Vencimento</th>
-                    <th>Nome</th>
-                    <th>Valor</th>
-                    <th>Paga?</th>
-                    <th>Ações</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr v-for="(index, o) in bills">
-                    <td>{{ index + 1 }}</td>
-                    <td>{{ o.date_due }}</td>
-                    <td>{{ o.name }}</td>
-                    <td>{{ o.value | currency 'R$ ' 2 }}</td>
-                    <td class="my-class" :class="{'green': o.done, 'red': !o.done}">
-                        <div v-if="o.done === 1">
-                            <a href="#" @click.prevent="payBill(o)">{{ o.done | doneLabel }}</a>
-                        </div>
-                        <div v-else>
-                            <a href="#" @click.prevent="payBill(o)">{{ o.done | doneLabel }}</a>
-                        </div>
-                    </td>
-                    <td>
-                        <a href="#" @click.prevent="loadBill(o)">Editar</a> |
-                        <a href="#" @click.prevent="deleteBill(o)">Excluir</a>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
+            <bill-list-component></bill-list-component>
         </div>
         
         <div v-if="activedView == 1">
@@ -126,15 +166,6 @@ var appComponent = Vue.extend({
                 'Cartão de crédito',
                 'Empréstimo',
                 'Gasolina'
-            ],
-            bills: [
-                {date_due: '11/08/16', name: 'Conta de água', value: 55.99, done: true},
-                {date_due: '11/08/16', name: 'Conta de luz', value: 130.95, done: false},
-                {date_due: '15/08/16', name: 'Conta de telefone', value: 75.95, done: false},
-                {date_due: '15/08/16', name: 'Cartão de crédito', value: 1350.85, done: false},
-                {date_due: '20/08/16', name: 'Empréstimo', value: 2000.15, done: false},
-                {date_due: '20/08/16', name: 'Supermecado', value: 650.45, done: false},
-                {date_due: '20/08/16', name: 'Gasolina', value: 150.25, done: false},
             ]
         };
     },
@@ -164,23 +195,6 @@ var appComponent = Vue.extend({
                 done: false
             };
             this.activedView = 0;
-        },
-        loadBill: function (bill) {
-            this.bill = bill;
-            this.activedView = 1;
-            this.formType = 'update';
-        },
-        deleteBill: function (bill) {
-            if (confirm('Deseja excluir está conta?')) {
-                this.bills.$remove(bill);
-            }
-        },
-        payBill: function (bill) {
-            var message = (bill.done == 1) ? "Deseja mudar o status desta conta para 'Não paga'?"
-                : "Deseja mudar o status desta conta para 'Paga'?";
-            if (confirm(message)) {
-                bill.done = !bill.done;
-            }
         }
     }
 });
