1cd3dfc8737299f7902ccf39bd8f8430483d10da
netoudi <netoudi@rocketmail.com>
2016-08-24 12:24:16 -0300

    Criando componente central de acesso a dados.

--- vuejs/bill-list.component.js
+++ vuejs/bill-list.component.js
@@ -35,15 +35,7 @@ window.billListComponent = Vue.extend({
     `,
     data: function () {
         return {
-            bills: [
-                {date_due: '11/08/16', name: 'Conta de água', value: 55.99, done: true},
-                {date_due: '11/08/16', name: 'Conta de luz', value: 130.95, done: false},
-                {date_due: '15/08/16', name: 'Conta de telefone', value: 75.95, done: false},
-                {date_due: '15/08/16', name: 'Cartão de crédito', value: 1350.85, done: false},
-                {date_due: '20/08/16', name: 'Empréstimo', value: 2000.15, done: false},
-                {date_due: '20/08/16', name: 'Supermecado', value: 650.45, done: false},
-                {date_due: '20/08/16', name: 'Gasolina', value: 150.25, done: false},
-            ]
+            bills: this.$root.$children[0].bills
         };
     },
     methods: {
--- vuejs/main.js
+++ vuejs/main.js
@@ -1,5 +1,25 @@
 var router = new VueRouter();
 
+var mainComponent = Vue.extend({
+    components: {
+        'app-component': appComponent
+    },
+    template: `<app-component></app-component>`,
+    data: function () {
+        return {
+            bills: [
+                {date_due: '11/08/16', name: 'Conta de água', value: 55.99, done: true},
+                {date_due: '11/08/16', name: 'Conta de luz', value: 130.95, done: false},
+                {date_due: '15/08/16', name: 'Conta de telefone', value: 75.95, done: false},
+                {date_due: '15/08/16', name: 'Cartão de crédito', value: 1350.85, done: false},
+                {date_due: '20/08/16', name: 'Empréstimo', value: 2000.15, done: false},
+                {date_due: '20/08/16', name: 'Supermecado', value: 650.45, done: false},
+                {date_due: '20/08/16', name: 'Gasolina', value: 150.25, done: false},
+            ]
+        };
+    }
+});
+
 router.map({
     '/bills': {
         name: 'bill.list',
@@ -16,7 +36,7 @@ router.map({
 
 router.start({
     components: {
-        'app-component': appComponent
+        'main-component': mainComponent
     }
 }, '#app');
 
