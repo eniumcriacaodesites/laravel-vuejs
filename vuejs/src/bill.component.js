4038eb0065abac4b50526a49835b305293544018
netoudi <netoudi@rocketmail.com>
2016-11-03 20:43:39 -0200

    Menu lateral.

--- vuejs/src/bill.component.js
+++ vuejs/src/bill.component.js
@@ -4,7 +4,15 @@ window.billComponent = Vue.extend({
             <nav>
                 <div class="nav-wrapper container">
                     <div class="brand-logo right">CodeBills</div>
-                    <ul id="nav-mobile" class="left">
+                    <a href="#" data-activates="nav-mobile" class="button-collapse">
+                        <i class="material-icons">menu</i>
+                    </a>
+                    <ul class="left hide-on-med-and-down">
+                        <li v-for="o in menus">
+                            <a v-link="{name: o.routeName}">{{ o.name }}</a>
+                        </li>
+                    </ul>
+                    <ul id="nav-mobile" class="side-nav">
                         <li v-for="o in menus">
                             <a v-link="{name: o.routeName}">{{ o.name }}</a>
                         </li>
@@ -14,6 +22,11 @@ window.billComponent = Vue.extend({
         </div>
         <router-view></router-view>
     `,
+    created() {
+        $(document).ready(function () {
+            $('.button-collapse').sideNav();
+        });
+    },
     data() {
         return {
             menus: [
