0ca048e60f48b31fb18d33b5ef28baa3a5db7f14
netoudi <netoudi@rocketmail.com>
2016-11-15 09:45:11 -0200

    Refatorando formulários.

--- vuejs/src/js/bill-pay/bill-pay-create.component.js
+++ vuejs/src/js/bill-pay/bill-pay-create.component.js
@@ -2,21 +2,43 @@ window.billPayCreateComponent = Vue.extend({
     template: `
         <div class="container">
             <form name="form" @submit.prevent="submit">
-                <label>Vencimento:</label>
-                <input type="text" v-model="bill.date_due | dateFormat 'pt-br'">
-                <br><br>
-                <label>Nome:</label>
-                <select v-model="bill.name">
-                    <option v-for="o in names" :value="o">{{ o | textFormat }}</option>
-                </select>
-                <br><br>
-                <label>Valor:</label>
-                <input type="text" v-model="bill.value | numberFormat 'pt-br'">
-                <br><br>
-                <label>Pago?</label>
-                <input type="checkbox" v-model="bill.done">
-                <br><br>
-                <input type="submit" value="Enviar">
+                <div class="row">
+                    <div class="input-field col s6">
+                        <label class="active">Vencimento:</label>
+                        <input type="text" v-model="bill.date_due | dateFormat 'pt-br'" placeholder="Informe a data">
+                    </div>
+                    <div class="input-field col s6">
+                        <label class="active">Valor:</label>
+                        <input type="text" v-model="bill.value | numberFormat 'pt-br'">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s6">
+                        <label class="active">Nome:</label>
+                        <select v-model="bill.name" class="browser-default">
+                            <option value="" disabled selected>Escolha um nome</option>
+                            <option v-for="o in names" :value="o">{{ o | textFormat }}</option>
+                        </select>
+                    </div>
+                    <div class="input-field col s6">
+                        <label class="active">Pago?</label>
+                        <div class="switch">
+                            <label>
+                                Off
+                                <input type="checkbox" v-model="bill.done">
+                                <span class="lever"></span>
+                                On
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s12">
+                        <button class="btn waves-effect waves-light right" type="submit" name="action">Enviar
+                            <i class="material-icons right">send</i>
+                        </button>
+                    </div>
+                </div>
             </form>
         </div>
     `,
