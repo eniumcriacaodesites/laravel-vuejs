4bd35301c9792d8c8ef6d2e39f87e8b79be2fdb8
netoudi <netoudi@rocketmail.com>
2016-11-15 18:05:48 -0200

    Fase - Integrando com o Materialize CSS.

--- vuejs/src/js/bill.component.js
+++ vuejs/src/js/bill.component.js
@@ -1,34 +1,38 @@
 window.billComponent = Vue.extend({
     template: `
-        <ul v-bind:id="o.id" class="dropdown-content" v-for="o in menusDropdown">
-            <li v-for="item in o.items">
-                <a v-link="{name: item.routeName}">{{ item.name }}</a>
-            </li>
-        </ul>
-        <div class="navbar-fixed">
-            <nav>
-                <div class="nav-wrapper container">
-                    <div class="brand-logo right">CodeBills</div>
-                    <a href="#" data-activates="nav-mobile" class="button-collapse">
-                        <i class="material-icons">menu</i>
-                    </a>
-                    <ul class="left hide-on-med-and-down">
-                        <li v-for="o in menus">
-                            <a v-if="o.dropdownId" class="dropdown-button" href="!#" v-bind:data-activates="o.dropdownId">
-                                {{ o.name }} <i class="material-icons right">arrow_drop_down</i>
-                            </a>
-                            <a v-else v-link="{name: o.routeName}">{{ o.name }}</a>
-                        </li>
-                    </ul>
-                    <ul id="nav-mobile" class="side-nav">
-                        <li v-for="o in menus">
-                            <a v-link="{name: o.routeName}">{{ o.name }}</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-        <router-view></router-view>
+        <header>
+            <ul v-bind:id="o.id" class="dropdown-content" v-for="o in menusDropdown">
+                <li v-for="item in o.items">
+                    <a v-link="{name: item.routeName}">{{ item.name }}</a>
+                </li>
+            </ul>
+            <div class="navbar-fixed">
+                <nav>
+                    <div class="nav-wrapper container">
+                        <div class="brand-logo right">CodeBills</div>
+                        <a href="#" data-activates="nav-mobile" class="button-collapse">
+                            <i class="material-icons">menu</i>
+                        </a>
+                        <ul class="left hide-on-med-and-down">
+                            <li v-for="o in menus">
+                                <a v-if="o.dropdownId" class="dropdown-button" href="!#" v-bind:data-activates="o.dropdownId">
+                                    {{ o.name }} <i class="material-icons right">arrow_drop_down</i>
+                                </a>
+                                <a v-else v-link="{name: o.routeName}">{{ o.name }}</a>
+                            </li>
+                        </ul>
+                        <ul id="nav-mobile" class="side-nav">
+                            <li v-for="o in menus">
+                                <a v-link="{name: o.routeName}">{{ o.name }}</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </header>
+        <main>
+            <router-view></router-view>
+        </main>
         <footer>
             <h6 class="center-align">&copy; 2016 - CodeBills</h6>
         </footer>
