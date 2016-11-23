79d2410abd9994aac2e45b7bb194443a48e65de7
netoudi <netoudi@rocketmail.com>
2016-11-23 14:51:42 -0200

    Criando método para criar token.

---
+++ code-bills/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace CodeBills\Http\Controllers\Api;
+
+use CodeBills\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    use AuthenticatesUsers;
+
+    public function accessToken(Request $request)
+    {
+        $this->validateLogin($request);
+
+        $credentials = $this->credentials($request);
+
+        if ($token = Auth::guard('api')->attempt($credentials)) {
+            return $this->sendLoginResponse($request, $token);
+        }
+    }
+
+    protected function sendLoginResponse(Request $request, $token)
+    {
+        return response()->json([
+            'token' => $token,
+        ]);
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->flush();
+
+        $request->session()->regenerate();
+
+        return redirect(env('URL_ADMIN_LOGIN'));
+    }
+}
--- code-bills/app/User.php
+++ code-bills/app/User.php
@@ -4,8 +4,9 @@ namespace CodeBills;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -33,4 +34,30 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [
+            'user' => [
+                'id' => $this->id,
+                'name' => $this->name,
+                'email' => $this->email,
+            ],
+        ];
+    }
 }
