78a943f44ce921c69519c7bce69af6e2fa8bce98
netoudi <netoudi@rocketmail.com>
2016-11-28 10:14:12 -0200

    Criando layout do SPA

---
+++ code-bills/resources/assets/spa/js/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+        <h6 class="center-align">&copy; {{ year }} - CodeBills</h6>
+    </footer>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                year: new Date().getFullYear()
+            }
+        }
+    };
+</script>
---
+++ code-bills/resources/views/layouts/spa.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name', 'CodeBills') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/spa.css') }}" rel="stylesheet">
+</head>
+<body id="app">
+
+<app-component></app-component>
+
+<!-- Scripts -->
+<script src="{{ asset('build/spa.bundle.js') }}"></script>
+</body>
+</html>
