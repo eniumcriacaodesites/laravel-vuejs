a78c080d83219ce2e5ec8440ca349a77e0d5abb8
netoudi <netoudi@rocketmail.com>
2016-11-28 11:07:23 -0200

    Organizando rotas

--- code-bills/resources/assets/spa/js/spa.js
+++ code-bills/resources/assets/spa/js/spa.js
@@ -1,22 +1,3 @@
-import LoginComponent from "./components/Login.vue";
-import AppComponent from "./components/App.vue";
-
-require('materialize-css');
-
 window.Vue = require('vue');
-
-let VueRouter = require('vue-router');
-const router = new VueRouter();
-
-router.map({
-    '/login': {
-        name: 'auth.login',
-        component: LoginComponent
-    }
-});
-
-router.start({
-    components: {
-        'app-component': AppComponent
-    }
-}, '#app');
+require('materialize-css');
+require('./router');
