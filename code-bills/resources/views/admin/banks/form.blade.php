19eaf444e7aee7835099b9402856b0f1a7cdfa22
netoudi <netoudi@rocketmail.com>
2017-03-09 16:10:45 -0300

    Fase - Iniciando CRUD de Contas a Pagar e a Receber

--- code-bills/resources/views/admin/banks/form.blade.php
+++ code-bills/resources/views/admin/banks/form.blade.php
@@ -2,44 +2,59 @@
 
 @section('content')
     <div class="container">
-        <h3>{{ empty($bank) ? 'Adicionar Banco' : 'Atualizar Banco' }}</h3>
-
-        @if(!empty($bank))
-            {!! Form::model($bank, ['route' => ['admin.banks.update', $bank->id], 'method' => 'PUT', 'files' => true]) !!}
-        @else
-            {!! Form::open(['route' => 'admin.banks.store', 'files' => true]) !!}
-        @endif
-
-        @if (count($errors) > 0)
-            <div class="card-panel red white-text lighten-2">
-                @foreach ($errors->all() as $error)
-                    <span>{{ $error }}</span>
-                @endforeach
+        <div class="row">
+            <div class="card-panel green lighten-3">
+                <span class="green-text text-darken-2">
+                    <h5>{{ empty($bank) ? 'Adicionar Banco' : 'Atualizar Banco' }}</h5>
+                </span>
             </div>
-        @endif
 
-        <div class="form-group">
-            {!! Form::label('name', 'Name:') !!}
-            {!! Form::text('name', null, ['class' => 'form-control']) !!}
-        </div>
+            <div class="card-panel z-depth-2">
+                @if(!empty($bank))
+                    {!! Form::model($bank, ['route' => ['admin.banks.update', $bank->id], 'method' => 'PUT', 'files' => true]) !!}
+                @else
+                    {!! Form::open(['route' => 'admin.banks.store', 'files' => true]) !!}
+                @endif
 
-        <div class="form-group">
-            <div class="file-field input-field">
-                <div class="btn">
-                    <span>Logo</span>
-                    <input type="file" id="logo" name="logo">
+                @if (count($errors) > 0)
+                    <div class="card-panel red white-text lighten-2">
+                        @foreach ($errors->all() as $error)
+                            <span>{{ $error }}</span>
+                        @endforeach
+                    </div>
+                @endif
+
+                <div class="row">
+                    <div class="input-field col s12">
+                        {!! Form::label('name', 'Name:') !!}
+                        {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Informe o banco']) !!}
+                    </div>
                 </div>
-                <div class="file-path-wrapper">
-                    <input class="file-path validate" type="text">
+
+                <div class="row">
+                    <div class="input-field col s12">
+                        <div class="file-field input-field">
+                            <div class="btn">
+                                <span>Logo</span>
+                                <input type="file" id="logo" name="logo">
+                            </div>
+                            <div class="file-path-wrapper">
+                                <input class="file-path validate" type="text" placeholder="Selecione a logo do banco">
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
+                <div class="row">
+                    <div class="col s12 right-align">
+                        {!! Form::submit('Enviar', ['class'=>'btn btn-primary btn-sm']) !!}
+                        <a class="btn-cancel" href="{{ route('admin.banks.index') }}"
+                           role="button">Cancelar</a>
+                    </div>
+                </div>
+
+                {!! Form::close() !!}
             </div>
         </div>
-
-        <div class="form-group text-right">
-            {!! Form::submit('Enviar', ['class'=>'btn btn-primary btn-sm']) !!}
-            <a class="btn btn-warning btn-sm" href="{{ route('admin.banks.index') }}" role="button">Cancelar</a>
-        </div>
-
-        {!! Form::close() !!}
     </div>
 @endsection
--- code-bills/resources/views/admin/banks/index.blade.php
+++ code-bills/resources/views/admin/banks/index.blade.php
@@ -2,95 +2,107 @@
 
 @section('content')
     <div class="container">
-        <h3>Bancos</h3>
-
-        <a href="{{ route('admin.banks.create') }}" class="btn btn-default btn-sm pull-right">Novo Banco</a>
-
-        <br><br>
-
-        <form name="form-search" method="get" action="{{ route('admin.banks.index') }}">
-            <div class="filter-group">
-                <button type="submit" class="btn waves-effect">
-                    <i class="material-icons">search</i>
-                </button>
-                <div class="filter-wrapper">
-                    <input type="text" name="search" value="{{ $search }}" placeholder="Digite aqui a sua busca">
-                </div>
+        <div class="row">
+            <div class="card-panel green lighten-3">
+                <span class="green-text text-darken-2">
+                    <h5>Bancos</h5>
+                </span>
             </div>
-        </form>
 
-        <table class="bordered striped highlight responsive-table">
-            <thead>
-            <tr>
-                <th width="5%">
-                    <a href="{{ route('admin.banks.index', ['orderBy' => 'id', 'sortedBy' => ($sortedBy == 'asc') ? 'desc' : 'asc', 'search' => $search]) }}">
-                        ID
-                        @if($orderBy == 'id')
-                            <i class="material-icons right">
-                                {{ $sortedBy == 'asc' ? 'arrow_drop_up' : 'arrow_drop_down' }}
-                            </i>
-                        @endif
-                    </a>
-                </th>
-                <th>
-                    <a href="{{ route('admin.banks.index', ['orderBy' => 'name', 'sortedBy' => ($sortedBy == 'asc') ? 'desc' : 'asc', 'search' => $search]) }}">
-                        Nome
-                        @if($orderBy == 'name')
-                            <i class="material-icons right">
-                                {{ $sortedBy == 'asc' ? 'arrow_drop_up' : 'arrow_drop_down' }}
-                            </i>
-                        @endif
-                    </a>
-                </th>
-                <th width="15%">
-                    Logo
-                </th>
-                <th width="10%">Action</th>
-            </tr>
-            </thead>
-            <tbody>
-            @forelse($banks as $bank)
-                <tr>
-                    <td>{{ $bank->id }}</td>
-                    <td>{{ $bank->name }}</td>
-                    <td><img src="{{ asset("storage/banks/images/{$bank->logo}") }}" height="30"></td>
-                    <td nowrap="nowrap">
-                        <a href="{{ route('admin.banks.edit', ['id' => $bank->id]) }}"
-                           class="btn btn-primary btn-xs">Editar</a>
-                        <delete-action action="{{ route('admin.banks.destroy', ['id' => $bank->id]) }}"
-                                       action-element="link-delete-{{ $bank->id }}"
-                                       csrf-token="{{ csrf_token() }}">
-                            <a id="link-modal-{{ $bank->id }}"
-                               href="#{{ 'modal-delete-' . $bank->id }}"
-                               class="btn btn-danger btn-xs">Deletar</a>
-                            <modal :modal="{{ json_encode(['id' => 'modal-delete-' . $bank->id]) }}"
-                                   style="display: none;">
-                                <div slot="content">
-                                    <h5>Deseja excluir este banco?</h5>
-                                    <table class="bordered">
-                                        <tr>
-                                            <td>Nome:</td>
-                                            <td>{{ $bank->name }}</td>
-                                        </tr>
-                                    </table>
-                                </div>
-                                <div slot="footer">
-                                    <button class="btn-cancel modal-close modal-action">Cancel</button>
-                                    <button id="link-delete-{{ $bank->id }}" class="btn-ok modal-close modal-action">
-                                        Ok
-                                    </button>
-                                </div>
-                            </modal>
-                        </delete-action>
-                    </td>
-                </tr>
-                @empty
+            <div class="card-panel z-depth-2">
+                <form name="form-search" method="get" action="{{ route('admin.banks.index') }}">
+                    <div class="filter-group">
+                        <button type="submit" class="btn waves-effect">
+                            <i class="material-icons">search</i>
+                        </button>
+                        <div class="filter-wrapper">
+                            <input type="text" name="search" value="{{ $search }}"
+                                   placeholder="Digite aqui a sua busca">
+                        </div>
+                    </div>
+                </form>
+
+                <table class="bordered striped highlight responsive-table">
+                    <thead>
                     <tr>
-                        <td colspan="4">&raquo; Nenhum registro encontrado.</td>
+                        <th width="5%">
+                            <a href="{{ route('admin.banks.index', ['orderBy' => 'id', 'sortedBy' => ($sortedBy == 'asc') ? 'desc' : 'asc', 'search' => $search]) }}">
+                                ID
+                                @if($orderBy == 'id')
+                                    <i class="material-icons right">
+                                        {{ $sortedBy == 'asc' ? 'arrow_drop_up' : 'arrow_drop_down' }}
+                                    </i>
+                                @endif
+                            </a>
+                        </th>
+                        <th>
+                            <a href="{{ route('admin.banks.index', ['orderBy' => 'name', 'sortedBy' => ($sortedBy == 'asc') ? 'desc' : 'asc', 'search' => $search]) }}">
+                                Nome
+                                @if($orderBy == 'name')
+                                    <i class="material-icons right">
+                                        {{ $sortedBy == 'asc' ? 'arrow_drop_up' : 'arrow_drop_down' }}
+                                    </i>
+                                @endif
+                            </a>
+                        </th>
+                        <th width="10%">Action</th>
                     </tr>
-                @endforelse
-            </tbody>
-        </table>
-        {!! $banks->links() !!}
+                    </thead>
+                    <tbody>
+                    @forelse($banks as $bank)
+                        <tr>
+                            <td>{{ $bank->id }}</td>
+                            <td>
+                                <table>
+                                    <tr>
+                                        <td width="22">
+                                            <img src="{{ asset("storage/banks/images/{$bank->logo}") }}"
+                                                 class="bank-logo">
+                                        </td>
+                                        <td>{{ $bank->name }}</td>
+                                    </tr>
+                                </table>
+                            </td>
+                            <td nowrap="nowrap">
+                                <a href="{{ route('admin.banks.edit', ['id' => $bank->id]) }}"
+                                   class="btn btn-primary btn-xs">Editar</a>
+                                <delete-action action="{{ route('admin.banks.destroy', ['id' => $bank->id]) }}"
+                                               action-element="link-delete-{{ $bank->id }}"
+                                               csrf-token="{{ csrf_token() }}">
+                                    <a id="link-modal-{{ $bank->id }}"
+                                       href="#{{ 'modal-delete-' . $bank->id }}"
+                                       class="btn btn-danger btn-xs">Deletar</a>
+                                    <modal :modal="{{ json_encode(['id' => 'modal-delete-' . $bank->id]) }}"
+                                           style="display: none;">
+                                        <div slot="content">
+                                            <h5>Deseja excluir este banco?</h5>
+                                            <table class="bordered">
+                                                <tr>
+                                                    <td>Nome:</td>
+                                                    <td>{{ $bank->name }}</td>
+                                                </tr>
+                                            </table>
+                                        </div>
+                                        <div slot="footer">
+                                            <button class="btn-cancel modal-close modal-action">Cancel</button>
+                                            <button id="link-delete-{{ $bank->id }}"
+                                                    class="btn-ok modal-close modal-action">
+                                                Ok
+                                            </button>
+                                        </div>
+                                    </modal>
+                                </delete-action>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="4">&raquo; Nenhum registro encontrado.</td>
+                        </tr>
+                    @endforelse
+                    </tbody>
+                </table>
+                {!! $banks->links() !!}
+            </div>
+        </div>
     </div>
 @endsection
