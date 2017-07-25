958f26e8813f5964640c7625247be52b43cb6968
netoudi <netoudi@rocketmail.com>
2017-07-25 11:46:59 -0300

    Download do CSV de fluxo de caixa

--- code-bills/resources/assets/spa/js/components/cash-flow/CashFlowList.vue
+++ code-bills/resources/assets/spa/js/components/cash-flow/CashFlowList.vue
@@ -7,73 +7,80 @@
 
             <div class="card-panel z-depth-2">
                 <div v-if="hasCashFlows">
-                    <table class="bordered highlight responsive-table">
+                    <table class="bordered highlight responsive-table grey-text text-darken-3">
                         <thead>
                         <tr class="blue lighten-4">
-                            <th></th>
-                            <th v-if="!hasFirstMonthYear">
+                            <th class="text-csv"></th>
+                            <th v-if="!hasFirstMonthYear" class="text-csv">
                                 {{ firstMonthYear | monthYear }}
                             </th>
-                            <th v-for="o in monthsList" :class="{'blue lighten-2': isCurrentMonthYear(o.month_year)}">
+                            <th v-for="o in monthsList" :class="{'blue lighten-2': isCurrentMonthYear(o.month_year)}"
+                                class="text-csv">
                                 {{ o.month_year | monthYear }}
                             </th>
                         </tr>
                         </thead>
                         <tbody>
                         <tr class="blue lighten-2">
-                            <td class="tb-title">Saldo final</td>
-                            <td>{{ firstBalance }}</td>
-                            <td>{{ secondBalance }}</td>
-                            <td v-for="(key, o) in monthsListBalanceFinal">
+                            <td class="tb-title text-csv">Saldo final</td>
+                            <td class="text-csv">{{ firstBalance }}</td>
+                            <td class="text-csv">{{ secondBalance }}</td>
+                            <td v-for="(key, o) in monthsListBalanceFinal" class="text-csv">
                                 {{ balance(key) }}
                             </td>
                         </tr>
                         <tr>
-                            <td>Saldo mês anterior</td>
-                            <td>{{ balanceBeforeFirstMonth }}</td>
-                            <td>{{ firstBalance }}</td>
-                            <td>{{ secondBalance }}</td>
-                            <td v-for="(key, o) in monthsListBalancePrevious">
+                            <td class="text-csv">Saldo mês anterior</td>
+                            <td class="text-csv">{{ balanceBeforeFirstMonth }}</td>
+                            <td class="text-csv">{{ firstBalance }}</td>
+                            <td class="text-csv">{{ secondBalance }}</td>
+                            <td v-for="(key, o) in monthsListBalancePrevious" class="text-csv">
                                 {{ balance(key) }}
                             </td>
                         </tr>
                         <tr>
-                            <td>Geração de caixa</td>
-                            <td v-if="!hasFirstMonthYear">0</td>
-                            <td v-for="(key, o) in monthsList">
+                            <td class="text-csv">Geração de caixa</td>
+                            <td v-if="!hasFirstMonthYear" class="text-csv">0</td>
+                            <td v-for="(key, o) in monthsList" class="text-csv">
                                 {{ o.revenues.total - o.expenses.total }}
                             </td>
                         </tr>
                         <tr class="blue lighten-4">
-                            <td class="tb-title">Recebimentos</td>
-                            <td v-if="!hasFirstMonthYear">0</td>
-                            <td v-for="(key, o) in monthsList">
+                            <td class="tb-title text-csv">Recebimentos</td>
+                            <td v-if="!hasFirstMonthYear" class="text-csv">0</td>
+                            <td v-for="(key, o) in monthsList" class="text-csv">
                                 {{ o.revenues.total }}
                             </td>
                         </tr>
                         <tr v-for="o in categoriesMonths.revenues.data">
-                            <td>{{ o.name }}</td>
-                            <td v-if="!hasFirstMonthYear"></td>
-                            <td v-for="v in monthsList">
+                            <td class="text-csv">{{ o.name }}</td>
+                            <td v-if="!hasFirstMonthYear" class="text-csv"></td>
+                            <td v-for="v in monthsList" class="text-csv">
                                 {{ categoryTotal(o, v.month_year).total }}
                             </td>
                         </tr>
                         <tr class="blue lighten-4">
-                            <td class="tb-title">Pagamentos</td>
-                            <td v-if="!hasFirstMonthYear">0</td>
-                            <td v-for="(key, o) in monthsList">
+                            <td class="tb-title text-csv">Pagamentos</td>
+                            <td v-if="!hasFirstMonthYear" class="text-csv">0</td>
+                            <td v-for="(key, o) in monthsList" class="text-csv">
                                 {{ o.expenses.total }}
                             </td>
                         </tr>
                         <tr v-for="o in categoriesMonths.expenses.data">
-                            <td>{{ o.name }}</td>
-                            <td v-if="!hasFirstMonthYear"></td>
-                            <td v-for="v in monthsList">
+                            <td class="text-csv">{{ o.name }}</td>
+                            <td v-if="!hasFirstMonthYear" class="text-csv"></td>
+                            <td v-for="v in monthsList" class="text-csv">
                                 {{ categoryTotal(o, v.month_year).total }}
                             </td>
                         </tr>
                         </tbody>
                     </table>
+
+                    <div class="fixed-action-btn">
+                        <a class="btn-floating btn-large" @click="downloadCsv">
+                            <i class="large material-icons">file_download</i>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -83,6 +90,7 @@
 <script type="text/javascript">
     import PageTitleComponent from '../PageTitle.vue';
     import store from '../../store/store';
+    import PapaParse from 'papaparse';
 
     export default {
         components: {
@@ -136,6 +144,35 @@
             },
             isCurrentMonthYear(monthYear) {
                 return this.$options.filters.monthYear(new Date) == this.$options.filters.monthYear(monthYear);
+            },
+            getCsv() {
+                let csvResult = [];
+                csvResult.push([]);
+
+                $('table thead .text-csv').each((key, item) => {
+                    csvResult[0].push($(item).text().trim());
+                });
+
+                $('table tbody tr').each((key, tr) => {
+                    csvResult.push([]);
+
+                    $(tr).find('.text-csv').each((k, element) => {
+                        csvResult[csvResult.length - 1].push($(element).text().trim());
+                    });
+                });
+
+                return PapaParse.unparse(csvResult);
+            },
+            downloadCsv() {
+                let anchor = $('<a/>');
+                anchor.css('display', 'none');
+                anchor.attr('download', 'fluxo-de-caixa.csv')
+                    .attr('target', '_blank')
+                    .attr('href', `data:text/csv;charset=UTF-8,${encodeURIComponent(this.getCsv())}`);
+                anchor.html('Download CSV');
+                $('body').append(anchor);
+                anchor[0].click();
+                anchor.remove();
             }
         }
     };
